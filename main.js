6bd9ef29841755d1ecac41367e110eb7d333f07f
Aryan2802 <77482515+Aryan2802@users.noreply.github.com>
2021-01-15 13:52:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var op = [
+    "nope", "lulled", "HHAhahahhahahah", "You mistaked", "Take this-gvgt s dhfnnnn"
+];
+var x = 0;
+
+function lul() {
+    x = Math.floor(Math.random() * 4) + 0;
+    document.getElementById("ha").innerHTML = op [x];
+}
